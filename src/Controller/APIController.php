d50608b685a1f531f5a22f515a401d51f82e66d9
kyllian-claveau <kyllian.claveau@icloud.com>
2025-01-27 16:21:36 +0100

    Put controller for mobile api

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -10,7 +10,6 @@ use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -30,6 +29,34 @@ class APIController
         $this->entityManager = $entityManager;
         $this->security = $security;
         $this->jwtManager = $jwtManager;
+    }
+
+    #[Route(path: '/api/login', name: 'api_login', methods: ['POST'])]
+    public function apiLogin(Request $request, APIController $APIController, UserRepository $userRepository, AuthenticationUtils $authenticationUtils): JsonResponse
+    {
+        // Utilisation de la méthode pour obtenir l'utilisateur à partir du token
+        $user = $APIController->getUserFromToken($request, $userRepository);
+
+        if ($user instanceof User) {
+            $token = $this->jwtManager->create($user);
+            return new JsonResponse([
+                'message' => 'Authentification réussie',
+                'token' => $token
+            ], JsonResponse::HTTP_OK);
+
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        if ($error instanceof AuthenticationException) {
+            return new JsonResponse([
+                'message' => 'Identifiants invalides',
+            ], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        return new JsonResponse([
+            'message' => 'Erreur lors de la connexion',
+        ], JsonResponse::HTTP_BAD_REQUEST);
     }
 
     #[Route('/api/user/{id}/reservations', name: 'api_user_reservation_list', methods: ['POST'])]
@@ -82,34 +109,6 @@ class APIController
             ];
         }
         return new JsonResponse($reservationData, 200);
-    }
-
-    #[Route(path: '/api/login', name: 'api_login', methods: ['POST'])]
-    public function apiLogin(Request $request, APIController $APIController, UserRepository $userRepository, AuthenticationUtils $authenticationUtils): JsonResponse
-    {
-        // Utilisation de la méthode pour obtenir l'utilisateur à partir du token
-        $user = $APIController->getUserFromToken($request, $userRepository);
-
-        if ($user instanceof User) {
-            $token = $this->jwtManager->create($user);
-            return new JsonResponse([
-                'message' => 'Authentification réussie',
-                'token' => $token
-            ], JsonResponse::HTTP_OK);
-
-        }
-
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        if ($error instanceof AuthenticationException) {
-            return new JsonResponse([
-                'message' => 'Identifiants invalides',
-            ], JsonResponse::HTTP_UNAUTHORIZED);
-        }
-
-        return new JsonResponse([
-            'message' => 'Erreur lors de la connexion',
-        ], JsonResponse::HTTP_BAD_REQUEST);
     }
 
     public function getUserFromToken(Request $request, UserRepository $userRepository)
