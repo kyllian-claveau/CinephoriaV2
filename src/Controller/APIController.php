ec300f62bd518a0bc43f0447e82297f78de11872
kyllian-claveau <kyllian.claveau@icloud.com>
2025-01-27 16:15:46 +0100

    Put controller for mobile api

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
@@ -15,12 +18,70 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class APIController
 {
     private $jwtManager;
+    private $entityManager;
+    private $security;
 
     public function __construct(
         JWTTokenManagerInterface $jwtManager,
+        EntityManagerInterface $entityManager,
+        Security $security
     )
     {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
         $this->jwtManager = $jwtManager;
+    }
+
+    #[Route('/api/user/{id}/reservations', name: 'api_user_reservation_list', methods: ['POST'])]
+    public function list(int $id, Request $request, UserRepository $userRepository): JsonResponse
+    {
+        $content = json_decode($request->getContent(), true);
+        $token = $content['token'] ?? null;
+
+        if (!$token) {
+            return new JsonResponse(['message' => 'Invalid credentials.'], 401);
+        }
+
+        try {
+            $decodedToken = $this->jwtManager->decode($token);
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => 'Invalid token.'], 401);
+        }
+
+        if (!$decodedToken) {
+            return new JsonResponse(['message' => 'Invalid token.'], 401);
+        }
+
+        $userId = $decodedToken['id'];
+
+        $user = $userRepository->find($userId);
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'Invalid credentials.'], 401);
+        }
+
+        if ($user->getId() !== $id) {
+            return new JsonResponse(['message' => 'You do not have access to this resource.'], 403);
+        }
+
+        $reservations = $this->entityManager->getRepository(Reservation::class)->findBy(['user' => $id]);
+
+        $reservationData = [];
+        foreach ($reservations as $reservation) {
+            $reservationData[] = [
+                'id' => $reservation->getId(),
+                'user_id' => $reservation->getUser()->getId(),
+                'user_firstname' => $reservation->getUser()->getFirstname(),
+                'user_lastname' => $reservation->getUser()->getLastname(),
+                'total_price' => $reservation->getTotalPrice(),
+                'film' => $reservation->getSession()->getFilm()->getTitle(),
+                'room' => $reservation->getSession()->getRoom()->getNumber(),
+                'cinema' => $reservation->getSession()->getCinema()->getName(),
+                'start_date' => $reservation->getSession()->getStartDate()->format('Y-m-d'),
+                'end_date' => $reservation->getSession()->getEndDate() ? $reservation->getEndDate()->format('Y-m-d') : null,
+            ];
+        }
+        return new JsonResponse($reservationData, 200);
     }
 
     #[Route(path: '/api/login', name: 'api_login', methods: ['POST'])]
