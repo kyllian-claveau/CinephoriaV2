9f8d7b4ba574d4185c9588d545c6ab4c957b5403
kyllian-claveau <kyllian.claveau@icloud.com>
2025-01-31 09:10:51 +0100

    Correct script

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,13 @@
 import '../styles/app.css';
+
+    document.addEventListener("DOMContentLoaded", function () {
+    let startDateInput = document.getElementById("session_endDate");
+    let endDateInput = document.getElementById("session_endDate");
+
+    startDateInput.addEventListener("change", function () {
+    endDateInput.min = startDateInput.value;
+    if (endDateInput.value < startDateInput.value) {
+    endDateInput.value = startDateInput.value;
+}
+});
+});
