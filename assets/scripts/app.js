d972e20670199d775d66e3ad2287ba62ac7419c6
kyllian-claveau <kyllian.claveau@icloud.com>
2025-02-05 22:18:21 +0100

    Resolve console issue

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,20 +1,22 @@
 import '../styles/app.css';
-
-    document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     let startDateInput = document.getElementById("session_startDate");
     let endDateInput = document.getElementById("session_endDate");
 
-    startDateInput.addEventListener("change", function () {
-    let startDateTime = new Date(startDateInput.value);
+    // Vérifier si les éléments existent avant d'ajouter les écouteurs d'événements
+    if (startDateInput && endDateInput) {
+        startDateInput.addEventListener("change", function () {
+            let startDateTime = new Date(startDateInput.value);
 
-    if (!isNaN(startDateTime.getTime())) {
-    let minDateTime = startDateTime.toISOString().slice(0, 16);
-    endDateInput.min = minDateTime;
+            if (!isNaN(startDateTime.getTime())) {
+                let minDateTime = startDateTime.toISOString().slice(0, 16);
+                endDateInput.min = minDateTime;
 
-    let endDateTime = new Date(endDateInput.value);
-    if (endDateTime < startDateTime) {
-    endDateInput.value = minDateTime;
-}
-}
-});
+                let endDateTime = new Date(endDateInput.value);
+                if (endDateTime < startDateTime) {
+                    endDateInput.value = minDateTime;
+                }
+            }
+        });
+    }
 });
